7f26dd08b6f504086d25c710c27e5fb6c7029f51
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-17 14:16:18 -0600

    write user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
+const { getUsers, getSingleUser, createUser, updateUser, deleteUser } = require("../../controllers/user-controller");
+
+router.get("/", getUsers);
+
+router.get("/:userId", getSingleUser);
+
+router.post("/", createUser);
+
+router.put("/:userId", updateUser);
+
+router.delete("/:userId", deleteUser);
 
 module.exports = router;
