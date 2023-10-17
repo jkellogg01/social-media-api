950f8961227a4459b8435bee0f0f22d7cc792178
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-17 13:58:19 -0600

    write thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,20 @@
 const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought
+} = require("../../controllers/thought-controller");
+
+router.get("/", getThoughts);
+
+router.get("/:thoughtId", getSingleThought);
+
+router.post("/", createThought);
+
+router.put("/:thoughtId", updateThought);
+
+router.delete("/:thoughtId", deleteThought);
 
 module.exports = router;
