a609f4237c2e42e03dbf374154de6f6032831d70
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-17 13:49:43 -0600

    write router skeletons

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,8 @@
 const router = require("express").Router();
+const user = require("./user-routes");
+const thought = require("./thought-routes");
+
+router.use("/user", user);
+router.use("/thought", thought);
 
 module.exports = router;
