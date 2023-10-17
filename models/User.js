ea5ba97d4a7226f493c6bdb3d4e8453492133efe
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2023-10-17 17:13:48 -0600

    write user model

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const { Schema, Types, model } = require("mongoose");
+
+const userFields = {
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trimmed: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [isEmail, 'invalid email'],
+  },
+  thoughts: [{
+    type: Types.ObjectId,
+    ref: 'thought',
+  }],
+  friends: [{
+    type: Types.ObjectId,
+    ref: 'thought',
+  }],
+};
+
+const userOpts = {
+  toJSON: {
+    virtuals: true,
+  },
+};
+
+const userSchema = new Schema(userFields, userOpts);
+
+const User = model('user', userSchema);
+
+User.virtuals('friendCount').get(function () {
+  return this.friends.length;
+});
+
+module.exports = User;
